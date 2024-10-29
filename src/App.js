b3a83eba8ea187fc3c33d618d20b86820cc20df1
pki071120 <ddametyy@gmail.com>
2024-10-29 20:55:15 +0900

    Update Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Router from "./router";
+import React from "react";
+import { Temp } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
 
 function App() {
-	return <Router />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<div />} />
+				<Route path="/temp" element={<Temp />} />
+				<Route path="/signIn" element={<SignIn />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
