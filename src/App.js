11243fd71bd20c31fa222fb2144cd84066aca39d
pki071120 <ddametyy@gmail.com>
2024-10-31 17:44:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { Temp, Survey } from "./pages";
+import {
+	Temp,
+	SignUp,
+	Shopping,
+	Bookmark,
+	Search,
+	SignIn,
+	Home,
+	Write,
+} from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
+import { Footer } from "./components";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<div />} />
-				<Route path="*" element={<h1>hello</h1>} />
+				<Route path="*" element={<Footer />} />
 				<Route path="/temp" element={<Temp />} />
+				<Route path="/signIn" element={<SignIn />} />
 				<Route path="/signUp" element={<SignUp />} />
-				<Route path="/survey" element={<Survey />} />
+				<Route path="/shopping" element={<Shopping />} />
+				<Route path="/bookmark" element={<Bookmark />} />
+				<Route path="/search" element={<Search />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/write" element={<Write />} />
 			</Routes>
 		</BrowserRouter>
 	);
