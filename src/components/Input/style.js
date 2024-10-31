95c237382162fdcc6576be74741e1b0ab0cc09f2
pki071120 <ddametyy@gmail.com>
2024-10-31 17:45:49 +0900

    Update InputStyle

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,8 +1,16 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	display: flex;
+	width: 100%;
+`;
 
 export const Input = styled.input`
 	display: flex;
-	width: 100px;
+	width: 100%;
+	height: 100%;
+	font-size: 20px;
+	border: 0;
+	align-items: center;
+	outline: none;
 `;
