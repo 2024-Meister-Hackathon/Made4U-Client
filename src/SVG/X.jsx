6dda9837b71dda2438b9ffb40a73c265fdda96c7
pki071120 <ddametyy@gmail.com>
2024-10-31 17:51:16 +0900

    Add XSvg

---
+++ src/SVG/X.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const X = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			fill="none"
+		>
+			<rect width="20" height="20" />
+			<g mask="url(#mask0_82_178)">
+				<path
+					d="M5.33334 15.5447L4.45521 14.6665L9.12188 9.99987L4.45521 5.3332L5.33334 4.45508L10 9.12174L14.6667 4.45508L15.5448 5.3332L10.8781 9.99987L15.5448 14.6665L14.6667 15.5447L10 10.878L5.33334 15.5447Z"
+					fill="#1C1C1C"
+				/>
+			</g>
+		</svg>
+	);
+};
+
+export default X;
