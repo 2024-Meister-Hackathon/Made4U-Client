ee80ab33e4ce89ccff2f99fc1a74f62c7e8bac5e
pki071120 <ddametyy@gmail.com>
2024-10-31 17:46:47 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 export { default as Temp } from "./Temp";
+export { default as SignIn } from "./SignIn";
 export { default as SignUp } from "./SignUp";
-export { default as Survey } from "./Survey";
+export { default as Shopping } from "./Shopping";
+export { default as Bookmark } from "./Bookmark";
+export { default as Search } from "./Search";
+export { default as Home } from "./Home";
+export { default as Write } from "./Write";
