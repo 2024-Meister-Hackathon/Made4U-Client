112f0063d483ac30a3579ebd054d77f5273e51d4
pki071120 <ddametyy@gmail.com>
2024-10-31 17:44:46 +0900

    Update index.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Post } from "./Post";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Ingredient } from "./Ingredient";
+export { default as Advertise } from "./Advertise";
+export { default as SearchBar } from "./SearchBar";
