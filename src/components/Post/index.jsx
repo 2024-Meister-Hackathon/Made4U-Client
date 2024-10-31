ecd728982a731548354b01d71acf10aba3d88b84
pki071120 <ddametyy@gmail.com>
2024-10-31 17:45:59 +0900

    Update Post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
 import * as S from "./style";
-import Input from "../Input";
 import Ingredient from "../Ingredient";
+import { Bookmark, Cart, Comment, Recipe } from "../../SVG";
 
 const Post = () => {
-	const [comActive, setComActive] = useState(false);
 	const [recActive, setRecActive] = useState(false);
+	const [select, setSelect] = useState(false);
+
+	const materials = [
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+	];
+
+	const sources = [
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+		{
+			name: "재료이름",
+			unit: "1대",
+		},
+	];
+
+	const steps = ["hello", "world"];
 
 	return (
 		<>
@@ -15,19 +47,25 @@ const Post = () => {
 						<img src="" alt="프로필 사진" />
 						<h2>닉네임</h2>
 					</S.Profile>
-					<S.ButtonWrapper>
-						<S.Bookmark />
-						<S.Buy />
-					</S.ButtonWrapper>
+					<div onClick={() => setSelect(!select)}>
+						<Bookmark isSelect={select} />
+					</div>
 				</S.Title>
 				<S.Img>
-					<img src="" alt="음식사진" />
+					<img
+						src="https://cdn.discordapp.com/attachments/1300691924541509642/1301122547135545354/Frame2032.png?ex=6723544c&is=672202cc&hm=9232c2ec14b0aa79f79871d838683d440c1047f0e01cbf7aef3f2931a7872828&"
+						alt="음식사진"
+					/>
 				</S.Img>
 				<S.Info>
-					<S.ButtonWrapper2>
-						<S.ComButton onClick={() => setComActive(true)} />
-						<S.RecButton onClick={() => setRecActive(true)} />
-					</S.ButtonWrapper2>
+					<S.ButtonWrapper>
+						<S.ComButton>
+							<Comment />
+						</S.ComButton>
+						<S.RecButton onClick={() => setRecActive(true)}>
+							<Recipe />
+						</S.RecButton>
+					</S.ButtonWrapper>
 					<S.IngredientWrapper>
 						<Ingredient type={"feed"} />
 						<Ingredient type={"feed"} />
@@ -39,24 +77,53 @@ const Post = () => {
 						<Ingredient type={"feed"} />
 						<Ingredient type={"feed"} />
 					</S.IngredientWrapper>
-					<S.Description></S.Description>
+					<S.Description>helloworld worldhello helloworld</S.Description>
 				</S.Info>
-				<S.Comment active={comActive}>
-					<div onClick={() => setComActive(false)}></div>
-					<div>
-						<ul>
-							<li>댓글 1</li>
-							<li>댓글 2</li>
-							<li>댓글 3</li>
-						</ul>
-						<Input placeholder={"댓글 달기"} type={"text"} />
-					</div>
-				</S.Comment>
-				<S.Recipe active={recActive}>
-					<div onClick={() => setRecActive(false)}></div>
-					<div></div>
-				</S.Recipe>
 			</S.Container>
+			<S.RecipeBox active={recActive}>
+				<S.Temp onClick={() => setRecActive(false)}></S.Temp>
+				<S.Recipe>
+					<S.Bar>
+						<span />
+						레시피
+					</S.Bar>
+					<S.Content>
+						<div style={{ marginBottom: "2rem", width: "100%" }}>
+							<S.SubTitle>재료</S.SubTitle>
+							{materials.map((item, idx) => (
+								<S.Material key={idx}>
+									{item.name}
+									<div>
+										{item.unit}
+										<Cart />
+									</div>
+								</S.Material>
+							))}
+						</div>
+						<div style={{ marginBottom: "2rem", width: "100%" }}>
+							<S.SubTitle>양념</S.SubTitle>
+							{sources.map((item, idx) => (
+								<S.Material key={idx}>
+									{item.name}
+									<div>
+										{item.unit}
+										<Cart />
+									</div>
+								</S.Material>
+							))}
+						</div>
+						<div style={{ marginBottom: "2rem", width: "100%" }}>
+							<S.SubTitle>조리순서</S.SubTitle>
+							{steps.map((item, idx) => (
+								<S.Material key={idx} des>
+									<h2>{idx + 1}</h2>
+									<p>{item}</p>
+								</S.Material>
+							))}
+						</div>
+					</S.Content>
+				</S.Recipe>
+			</S.RecipeBox>
 		</>
 	);
 };
--- src/components/Post/style.js
+++ src/components/Post/style.js
@@ -3,15 +3,17 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 500px;
+	width: 100%;
 	height: auto;
 `;
 
 export const Title = styled.div`
 	display: flex;
+	padding: 0.5rem 1.25rem;
 	align-items: center;
 	justify-content: space-between;
-	border: 1px solid;
+	gap: 0.5rem;
+	align-self: stretch;
 `;
 
 export const Profile = styled.div`
@@ -28,103 +30,147 @@ export const Profile = styled.div`
 	}
 `;
 
-export const ButtonWrapper = styled.div`
-	display: flex;
-	width: 20px;
-	height: 10px;
-`;
-
 export const Bookmark = styled.div`
 	width: 10px;
 	height: 10px;
 	background-color: red;
 `;
 
-export const Buy = styled.div`
-	width: 10px;
-	height: 10px;
-	background-color: blue;
-`;
-
 export const Img = styled.div`
 	width: 100%;
-	height: 500px;
-	border: 1px solid aqua;
+	height: auto;
+	img {
+		width: 100%;
+	}
 `;
 
 export const Info = styled.div`
 	width: 100%;
-	height: 300px;
-	border: 1px solid purple;
+	display: flex;
+	flex-direction: column;
+	align-items: start;
 `;
 
-export const ButtonWrapper2 = styled.div`
+export const ButtonWrapper = styled.div`
 	display: flex;
+	width: 90%;
+	padding: 1rem 1.22rem 0 1.22rem;
 	justify-content: space-between;
-	border: 1px solid green;
+	align-items: center;
 `;
 
 export const ComButton = styled.span`
 	display: flex;
-	background-color: red;
+	justify-content: center;
+	align-items: center;
 	width: 30px;
 	height: 30px;
 `;
 
 export const RecButton = styled.span`
 	display: flex;
-	background-color: blue;
+	justify-content: center;
+	align-items: center;
 	width: 30px;
 	height: 30px;
 `;
 
-export const Comment = styled.p`
+export const RecipeBox = styled.div`
 	display: ${({ active }) => (active ? "flex" : "none")};
 	flex-direction: column;
 	width: 100%;
 	height: 100%;
 	position: absolute;
 	bottom: 0;
+	overflow: hidden;
+`;
 
-	& > div:nth-child(1) {
-		height: 30%;
-		background-color: rgba(0, 0, 0, 0.5);
-	}
+export const Temp = styled.div`
+	height: 10rem;
+`;
 
-	& > div:nth-child(2) {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		align-items: center;
-		height: 70%;
-		background-color: green;
+export const Recipe = styled.ul`
+	display: flex;
+	flex-direction: column;
+	height: 33rem;
+	padding: 2rem 1rem 0 1rem;
+	background-color: white;
+	align-items: center;
+	border-radius: 15px 15px 0 0;
+	overflow-y: scroll;
+	::-webkit-scrollbar {
+		display: none;
 	}
 `;
 
-export const Recipe = styled.p`
-	display: ${({ active }) => (active ? "flex" : "none")};
-	flex-direction: column;
+export const Bar = styled.span`
+	display: flex;
 	width: 100%;
-	height: 100%;
-	position: absolute;
-	bottom: 0;
+	height: auto;
+	flex-direction: column;
+	align-items: center;
+	gap: 1rem;
+	background-color: white;
+	border-radius: 15px 15px 0 0;
 
-	& > div:nth-child(1) {
-		height: 30%;
-		background-color: rgba(0, 0, 0, 0.5);
+	span {
+		width: 3.75rem;
+		height: 0.375rem;
+		border-radius: 62.4375rem;
+		background: var(--gray-800, #363636);
+	}
+`;
+
+export const Content = styled.div`
+	width: 100%;
+	margin: 2rem 0;
+	width: 100%;
+`;
+
+export const SubTitle = styled.h2`
+	display: flex;
+	width: 100%;
+	color: #000;
+	padding: 10px 0;
+	border-bottom: 1px solid black;
+	font-size: 1.25rem;
+`;
+
+export const Material = styled.div`
+	display: flex;
+	width: 95%;
+	padding: 0.875rem 0.625rem 0.875rem 0.5rem;
+	justify-content: space-between;
+	align-items: center;
+	${({ des }) => (des ? "border: none;" : "border-bottom: 1px solid #888;")}
+	div {
+		display: flex;
+		width: 8.125rem;
+		justify-content: space-between;
+		align-items: center;
+		gap: 5rem;
 	}
 
-	& > div:nth-child(2) {
-		height: 70%;
-		background-color: yellow;
+	h2 {
+		font-size: 2rem;
 	}
 `;
 
 export const IngredientWrapper = styled.div`
 	display: flex;
-	width: auto;
+	width: 95vw;
 	overflow-x: scroll;
+	::-webkit-scrollbar {
+		display: none;
+	}
 	gap: 10px;
+	padding: 0.75rem 0 0.75rem 1.22rem;
 `;
 
-export const Description = styled.p``;
+export const Description = styled.p`
+	display: flex;
+	height: 2.75rem;
+	padding: 0 1.25rem;
+	color: #1c1c1c;
+	font-size: 0.875rem;
+`;
