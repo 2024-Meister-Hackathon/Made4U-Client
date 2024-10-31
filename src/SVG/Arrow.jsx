8c3fa97d5abf6cf558f61da136324b16c26b214e
pki071120 <ddametyy@gmail.com>
2024-10-31 17:49:44 +0900

    Add ArrowSvg

---
+++ src/SVG/Arrow.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Arrow = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="40"
+			height="40"
+			viewBox="0 0 40 40"
+			fill="none"
+		>
+			<rect width="40" height="40" />
+			<g mask="url(#mask0_46_49)">
+				<path
+					d="M16.5867 29.4227L7.16333 19.9998L16.5867 10.5769L18.3429 12.3844L11.9775 18.7498H32.8367V21.2498H11.9775L18.3429 27.6152L16.5867 29.4227Z"
+					fill="#1C1C1C"
+				/>
+			</g>
+		</svg>
+	);
+};
+
+export default Arrow;
