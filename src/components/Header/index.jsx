ec489a3c1a6399cf1542dc4ad7385c75780cdb9e
pki071120 <ddametyy@gmail.com>
2024-10-31 17:45:22 +0900

    Update Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import * as S from "./style";
+import { Bookmark, Cart } from "../../SVG";
 
 const Header = () => {
 	return (
-		<S.Container>
-			<S.NavWrapper>
-				<S.Nav to={"/feed"}>
-					<img src="" alt="로고" />
+		<S.NavWrapper>
+			<S.Nav to={"/feed"}>
+				<img
+					src="https://raw.githubusercontent.com/akns27/EssetStorage/refs/heads/master/black(backX).png"
+					alt="로고"
+				/>
+			</S.Nav>
+			<div>
+				<S.Nav to={"/bookmark"}>
+					<Bookmark />
 				</S.Nav>
-				<div>
-					<S.Nav to={"/bookmark"}>
-						<img src="" alt="북마크" />
-					</S.Nav>
-					<S.Nav to={"/shopping"}>
-						<img src="" alt="장바구니" />
-					</S.Nav>
-				</div>
-			</S.NavWrapper>
-		</S.Container>
+				<S.Nav to={"/shopping"}>
+					<Cart />
+				</S.Nav>
+			</div>
+		</S.NavWrapper>
 	);
 };
 
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,21 +1,30 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
-export const Container = styled.div``;
-
 export const NavWrapper = styled.div`
 	display: flex;
-	width: 100%;
+	position: fixed;
+	top: 0;
+	width: 21rem;
+	height: 50px;
 	justify-content: space-between;
+	align-items: center;
+	background-color: white;
+	padding: 0.5rem 1.25rem 0.3rem 1.25rem;
 `;
 
 export const Nav = styled(Link)`
-	width: 30px;
-	height: 30px;
+	width: 2rem;
+	height: 2rem;
 
 	img {
 		width: 30px;
 		height: 30px;
 		object-fit: cover;
+	}
+
+	svg {
+		width: 2rem;
+		height: 2rem;
 	}
 `;
