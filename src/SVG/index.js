06a1831981c8601f0b5344fc75d85740ac5592da
pki071120 <ddametyy@gmail.com>
2024-10-31 17:50:31 +0900

    Update index

---
+++ src/SVG/index.js
@@ -0,0 +1,12 @@
+export { default as Arrow } from "./Arrow.jsx";
+export { default as Google } from "./Google.jsx";
+export { default as Profile } from "./Profile.jsx";
+export { default as X } from "./X.jsx";
+export { default as Home } from "./Home.jsx";
+export { default as Search } from "./Search.jsx";
+export { default as Write } from "./Write.jsx";
+export { default as Subscribe } from "./Subscribe.jsx";
+export { default as Bookmark } from "./Bookmark.jsx";
+export { default as Comment } from "./Comment.jsx";
+export { default as Recipe } from "./Recipe.jsx";
+export { default as Cart } from "./Cart.jsx";
