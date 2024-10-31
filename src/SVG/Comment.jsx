115abdd76d00aed3aa3fd04879f5572c86781b87
pki071120 <ddametyy@gmail.com>
2024-10-31 17:50:07 +0900

    Add CommentSvg

---
+++ src/SVG/Comment.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Comment = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="29"
+			height="28"
+			viewBox="0 0 29 28"
+			fill="none"
+		>
+			<rect x="0.5" width="28" height="28" />
+			<g mask="url(#mask0_57_646)">
+				<path
+					d="M3.41663 24.5449V2.91663H25.5833V20.4166H7.54488L3.41663 24.5449ZM6.79996 18.6666H23.8333V4.66663H5.16663V20.2822L6.79996 18.6666Z"
+					fill="#1C1C1C"
+				/>
+			</g>
+		</svg>
+	);
+};
+
+export default Comment;
