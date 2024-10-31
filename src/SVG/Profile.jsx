76407fc0a8bbc85818b16fe6bdbce5dc32f402a4
pki071120 <ddametyy@gmail.com>
2024-10-31 17:50:40 +0900

    Add ProfileSvg

---
+++ src/SVG/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+	return (
+		<svg
+			width="130"
+			height="130"
+			viewBox="0 0 130 130"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<g id="Frame 16">
+				<rect width="130" height="130" rx="65" fill="#F5F5F5" />
+				<g id="edit">
+					<rect id="Bounding box" x="33" y="33" width="64" height="64" />
+					<g mask="url(#mask0_49_429)">
+						<path
+							id="edit_2"
+							d="M46.3333 83.6665H49.6973L76.9946 56.3692L73.6306 53.0052L46.3333 80.3025V83.6665ZM42.3333 87.6665V78.6412L80.3333 40.6719L89.2873 49.7899L51.3586 87.6665H42.3333ZM75.2833 54.7165L73.6306 53.0052L76.9946 56.3692L75.2833 54.7165Z"
+							fill="#4F4F4F"
+						/>
+					</g>
+				</g>
+			</g>
+		</svg>
+	);
+};
+
+export default Profile;
